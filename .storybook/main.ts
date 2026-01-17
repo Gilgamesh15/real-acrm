95ed46b538967558b77183d0edf44acf23f446dd
Gilgamesh15 <michalrawski@student.agh.edu.pl>
2026-01-17 12:31:35 +0100

    storybook resolved 2

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,14 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ["../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "storybook-addon-remix-react-router",
   ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
-  },
-  "staticDirs": [
-    "../public"
-  ]
+  framework: "@storybook/react-vite",
 };
 export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import "@fontsource-variable/inter";
+import "@fontsource-variable/tektur";
+import "@fontsource/poiret-one";
+import type { Preview } from "@storybook/react-vite";
+import { withRouter } from "storybook-addon-remix-react-router";
+
+import "../app/app.css";
+
+const preview: Preview = {
+  decorators: [
+    // Our application is always in dark mode
+    (Story) => {
+      document.documentElement.classList.add("dark");
+      return <Story />;
+    },
+    (Story, { parameters }) => {
+      if (parameters.layout === "page") {
+        return (
+          <div className="min-h-svh max-w-screen overflow-x-hidden flex flex-col">
+            <Story />
+          </div>
+        );
+      } else {
+        return <Story />;
+      }
+    },
+    withRouter,
+  ],
+};
+
+export default preview;
