e9d8c8ecfc9f1eb0c5f1ee710182439b03ba7691
Gilgamesh15 <mrawski675@gmail.com>
2026-02-18 18:42:51 +0100

    a

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,12 @@
-import { RouterContextProvider, createRequestHandler } from "react-router";
+import { Hono } from "hono";
+import { createRequestHandler } from "react-router";
 import * as build from "virtual:react-router/server-build";
 
-const handler = createRequestHandler(build);
+const app = new Hono();
 
-export default (req: Request) => handler(req, new RouterContextProvider());
+// Add any additional Hono middleware here
+
+const handler = createRequestHandler(build);
+app.mount("/", (req) => handler(req));
+
+export default app.fetch;
